a393d959aacefac099dc14786657773f9398bc3b
jiangang.steven.hao <jiangang.steven.hao@4b0cd7ca-616b-1e5e-870c-2d995f09472d>
2012-04-06 15:39:02 +0000

    some new codes for the focus analysis are added

--- trunk/pyRaytrace/decamspotPY.py
+++ trunk/pyRaytrace/decamspotPY.py
@@ -1,12 +1,19 @@
 #---this is a python scrit that encapsulate the raytracing code from steve to make it more interactive/scriptable. 
 # J Hao 3/28/2012 @ FNAL
 
-import numpy as np
-import pyfits as pf
-import pylab as pl
-import os
-from DECamCCD_def import *
-
+try:
+    import numpy as np
+    import pyfits as pf
+    import pylab as pl
+    import os
+    from DECamCCD_def import *
+    from sklearn.decomposition import PCA
+    from scipy.optimize import leastsq
+    import mahotas as mh
+    import scipy.ndimage as nd
+except ImportError:
+    print 'the required packages are: numpy, pyfits,pylab,scikit,scipy,mahotas'
+    raise
 #-----define the parameters --------
 # this parameter will be written into the header
 
@@ -24,7 +31,7 @@ z = 0.1
 output='temp.fit'
 #------------------------------
 
-def decamspot(xmm=None,ymm=None):
+def decamspot(xmm=None,ymm=None,seeing=0,npix=40,zenith=0,filter='g', theta=0., corrector='corrector',x=None,y=None,z=None):
     #---generating the .par file------
     file = open('temp.par','w')
     file.write('RAYPATTERN '+str(raypattern) +'\n')
@@ -49,19 +56,28 @@ def decamspot(xmm=None,ymm=None):
     os.system('raytrace-3.13/decamspot '+'temp.par')
     #---output the result as an image vector
     b=pf.getdata('temp.fit')
+    seeing = seeing/scale # convert arcseconds to pixel
+    b=nd.filters.gaussian_filter(b,(seeing,seeing))
+    hdr = pf.getheader('temp.fit')
     bb = b.reshape(npix*npix)
     pos = np.array([xmm,ymm])
-    return np.concatenate((pos,bb))
+    return np.concatenate((pos,bb)),hdr
 
 
-def genImgV(filename=None,Nstar=None,ccd=None):
+def genImgV(filename=None,Nstar=None,ccd=None,seeing=0,npix=40,zenith=0,filter='g', theta=0., corrector='corrector',x=None,y=None,z=None):
+    """
+    seeing is the rms in arcseconds
+    """
     datalist = []
+    hdrlist = []
     randfactor=np.array([-1,1])
     if ccd is None:
         for i in range(Nstar):
             xmm = np.random.rand()*225*randfactor[np.random.randint(0,2)]
             ymm = np.random.rand()*225*randfactor[np.random.randint(0,2)]
-            datalist.append(decamspot(xmm=xmm,ymm=ymm))
+            res = decamspot(xmm=xmm,ymm=ymm,seeing=seeing,npix=npix,zenith=zenith,filter=filter, theta=theta, corrector=corrector,x=x,y=y,z=z)
+            datalist.append(res[0])
+            hdrlist.append(res[1])
         data = np.array(datalist)
     else:
         for i in range(Nstar):
@@ -71,7 +87,9 @@ def genImgV(filename=None,Nstar=None,ccd=None):
             else:
                 xmm = np.random.rand()*15*randfactor[np.random.randint(0,2)] + ccd[1]
                 ymm = np.random.rand()*30*randfactor[np.random.randint(0,2)] + ccd[2]
-            datalist.append(decamspot(xmm=xmm,ymm=ymm))
+            res = decamspot(xmm=xmm,ymm=ymm,seeing=seeing,npix=npix,zenith=zenith,filter=filter, theta=theta, corrector=corrector,x=x,y=y,z=z)
+            datalist.append(res[0])
+            hdrlist.append(res[1])
         data = np.array(datalist)
     if filename is not None:
         hdu = pf.PrimaryHDU(data)
@@ -86,12 +104,13 @@ def genImgV(filename=None,Nstar=None,ccd=None):
         hdu.header.add_comment('X: '+str(x))
         hdu.header.add_comment('Y: '+str(y))
         hdu.header.add_comment('Z: '+str(z))
+        hdu.header.add_comment('Seeing: '+str(seeing))
         if os.path.exists(filename):
             os.system('rm '+filename)
             hdu.writeto(filename)
         else:
             hdu.writeto(filename)        
-    return data
+    return data,hdrlist
 
    
 def disImg(data=None,colorbar=True):
@@ -109,7 +128,7 @@ def disImg(data=None,colorbar=True):
     pl.ylim(0,size-1)
     pl.xlabel('Pixels')
     pl.ylabel('Pixels')
-    pl.title('x ='+str(round(xmm,4)) +', y = '+str(round(ymm,4)) + ' (mm)')
+    #pl.title('x ='+str(round(xmm,4)) +', y = '+str(round(ymm,4)) + ' (mm)')
     pl.grid(color='yellow')
 
 
@@ -150,20 +169,136 @@ def disImgCCD(imgV=None,ccd=None):
     return 'The image is done!'
 
 
-def imgCCDctr(ccd=None):
+def imgCCDctr(ccd=None,filter='g',seeing=0,z=None):
     xmm = ccd[1]
     ymm = ccd[2]
-    data = genImgV(Nstar=1, ccd = ccd)
+    res = genImgV(Nstar=1, ccd = ccd,seeing=seeing,z=z)
+    data = res[0]
+    xcen = res[1][0]['xcen']
+    ycen = res[1][0]['ycen']
     disImgCCD(data,ccd)
-    pl.figtext(0.2,0.8,'CCD: '+ccd[0], color='r',weight='bold')
+    pl.figtext(0.2,0.85,'CCD: '+ccd[0], color='r')
+    pl.figtext(0.2,0.8,'Filter: '+filter, color='r')
+    pl.figtext(0.2,0.75, 'xtarget: '+str(xmm) + ',  ytarget: '+str(ymm), color='r')
+    pl.figtext(0.2,0.7, 'xcen: '+str(xcen) + ',  ycen: '+str(ycen), color='r')
     return '---done!-----'
 
+def decompPCA(data=None,comp=None):
+    img = data[:,2:].T
+    size = np.sqrt(img.shape[0])
+    pca = PCA(n_components=20)
+    imgNew = pca.fit_transform(img)
+    pl.imshow(imgNew[:,comp-1].reshape(size,size),interpolation='spline16')
+    pl.title('The '+str(comp)+' PC')
+    pl.colorbar()
+    return pca.explained_variance_ratio_
 
-#-----main program to generate the image vector -----
-# need to specify the parameters here 
-#filename = '/home/jghao/research/decamFocus/PSFdata/imagevector_zenith_0_theta_0_z_1_small.fits'
-#Nstar = 700  # specify the number of PSF you want to generate. 700 should ensure each CCD got ~ 10 stars.
+def fitPCAimg(coef=None, data = None, maxcomp = None):
+    """
+    get the coefficients of an image in terms of the PCA eigen images
+    """
+    img = data[:,2:].T
+    size = np.sqrt(img.shape[0])
+    pca = PCA(n_components=20)
+    imgBasis = pca.fit_transform(img)
+    nimg = img.shape[1]
+    #def residule(basis=None,maxcomp=None):
 
-#genImgV(filename='test.fit',Nstar=10)
+def centroidChange(ccd=None, filter=None):
+    res = genImgV(Nstar=1,ccd=ccd,filter=filter)
+    data = res[0]
+    xcen = res[1][0]['xcen']
+    ycen = res[1][0]['ycen']
+    size = np.sqrt(len(data[0][2:]))
+    img = data[0][2:].reshape(size,size)
+    xmm = data[0][0]
+    ymm = data[0][1]
+    xcentroid,ycentroid = nd.center_of_mass(img)
+    return xcentroid, ycentroid, xcen, ycen
 
-#imgV = pf.getdata(filename)
+def addseeing(imgV=None,seeing=None):
+    img = imgV[0]
+
+    
+def decompZernike(ccd=None,seeing=0.,theta=0.,x=None,y=None,z=None,filter=None):
+    imgV=genImgV(Nstar=1, ccd = ccd,seeing=seeing,theta = theta,filter=filter,x=x,y=y,z=z)[0]
+    size = np.sqrt(len(imgV[0][2:]))
+    img = imgV[0][2:].reshape(size,size)        
+    coeff = mh.zernike.zernike_moments(img,30,degree = 3, cm=mh.center_of_mass(img))
+    coeff = coeff
+    names = ['piston','tilt','defocus','astigmatism','coma','trefoil']
+    res = dict(zip(names,coeff))
+    disImgCCD(imgV,ccd)
+    return res
+
+
+def centroidChangeband(side=None):
+    Nccd = len(side)
+    xmmg = np.zeros(Nccd)
+    ymmg = np.zeros(Nccd)
+    xceng = np.zeros(Nccd)
+    yceng = np.zeros(Nccd)
+    xmmr = np.zeros(Nccd)
+    ymmr = np.zeros(Nccd)
+    xcenr = np.zeros(Nccd)
+    ycenr = np.zeros(Nccd)
+    xmmi = np.zeros(Nccd)
+    ymmi = np.zeros(Nccd)
+    xceni = np.zeros(Nccd)
+    yceni = np.zeros(Nccd)
+    xmmz = np.zeros(Nccd)
+    ymmz = np.zeros(Nccd)
+    xcenz = np.zeros(Nccd)
+    ycenz = np.zeros(Nccd)
+    ccdname = []
+    for i in range(Nccd):
+        print i
+        xmmg[i],ymmg[i],xceng[i],yceng[i] = centroidChange(ccd=side[i], filter='g')
+        xmmr[i],ymmr[i],xcenr[i],ycenr[i] = centroidChange(ccd=side[i], filter='r')
+        xmmi[i],ymmi[i],xceni[i],yceni[i] = centroidChange(ccd=side[i], filter='i')
+        xmmz[i],ymmz[i],xcenz[i],ycenz[i] = centroidChange(ccd=side[i], filter='z')
+        ccdname.append(side[i][0])
+    xrg = xcenr - xceng
+    xig = xceni - xceng
+    xzg = xcenz - xceng
+    yrg = ycenr - yceng
+    yig = yceni - yceng
+    yzg = ycenz - yceng
+    pl.subplot(2,1,1)
+    pl.plot(xrg*1000./15.,'b.',label='r-band vs. g-band')
+    pl.plot(xig*1000./15.,'r.',label='i-band vs. g-band')
+    pl.plot(xzg*1000./15.,'g.',label='z-band vs. g-band')
+    pl.xticks(np.arange(Nccd),ccdname)
+    pl.xlabel('CCD position')
+    pl.ylabel('x centroid difference (Pixels)')
+    pl.legend(loc='best')
+    pl.hlines(0,-1,31,linestyle='dashed',colors='k')
+    pl.xlim(-1,31)
+    pl.ylim(-1.5,1.5)
+    pl.subplot(2,1,2)
+    pl.plot(yrg*1000./15.,'b.',label='r-band vs. g-band')
+    pl.plot(yig*1000./15.,'r.',label='i-band vs. g-band')
+    pl.plot(yzg*1000./15.,'g.',label='z-band vs. g-band')
+    pl.xticks(np.arange(Nccd),ccdname)
+    pl.xlabel('CCD position')
+    pl.ylabel('y centroid difference (Pixels)')
+    pl.legend(loc='best')
+    pl.hlines(0,-1,31,linestyle='dashed',colors='k')
+    pl.xlim(-1,31)
+    pl.ylim(-1.5,1.5)
+    return '--- done!---'
+
+
+
+if __name__ == '__main__':
+    # ----the centroid change for different filters -----
+    S=[S1,S2,S3,S4,S5,S6,S7,S8,S9,S10,S11,S12,S13,S14,S15,S16,S17,S18,S19,S20,S21,S22,S23,S24,S25,S26,S27,S28,S29,S30,S31]
+
+    N=[N1,N2,N3,N4,N5,N6,N7,N8,N9,N10,N11,N12,N13,N14,N15,N16,N17,N18,N19,N20,N21,N22,N23,N24,N25,N26,N27,N28,N29,N30,N31]
+    
+    pl.figure(figsize=(15,10))
+    centroidChangeband(S)
+    pl.savefig('/home/jghao/research/decamFocus/figures/Scentroid_band.png')
+    pl.figure(figsize=(15,10))
+    centroidChangeband(N)
+    pl.savefig('/home/jghao/research/decamFocus/figures/Ncentroid_band.png')
