fc9892d5a5e9716f0e0688089f24091e2e637e23
jiangang.steven.hao <jiangang.steven.hao@4b0cd7ca-616b-1e5e-870c-2d995f09472d>
2012-09-30 22:36:05 +0000

    masterBias updated

--- trunk/masterBias.py
+++ trunk/masterBias.py
@@ -11,6 +11,7 @@ sys.path.append('/usr/remote/user/sispi/jiangang/decam-fermi')
 import glob as gl
 from DECamCCD import *
 from DECamCCD_def import *
+import time
 
 if len(sys.argv) == 1:
     print 'syntax: '
@@ -18,17 +19,21 @@ if len(sys.argv) == 1:
     print 'example: masterBias decam 12345 12346 12347'
     print 'The resulting median image will be named as masterBias.fits'
 else:
+    startTime=time.time()
     filehead = sys.argv[1]
     nimg=len(sys.argv) - 2
-    hdu = pf.open(filehead+'_'+sys.argv[2]+'.fits.fz') # need to funpack first
+    hdu = pf.open(filehead+'_'+sys.argv[2]+'.fits') # need to funpack first
     for ext in range(1,63):
         print ext
         for j in range(0,nimg):
             b=[]
-            filename = filehead+'_'+sys.argv[2+j]+'.fits.fz'
+            filename = filehead+'_'+sys.argv[2+j]+'.fits'
             imgext = pf.getdata(filename,ext)
             imgosub = oscanSub(imgext)
             b.append(imgosub)
         hdu[ext].data=np.median(b,axis=0)
         hdu[ext].header.update('bzero',0)
+    endTime=time.time()
+    elapseTime=endTime-startTime
+    print '---elapsed time: ' + str(elapseTime)
     hdu.writeto('masterBias.fits')
