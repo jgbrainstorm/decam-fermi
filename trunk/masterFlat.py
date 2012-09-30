5464ea90595dec472b15d22cbea7fdf37342ae35
jiangang.steven.hao <jiangang.steven.hao@4b0cd7ca-616b-1e5e-870c-2d995f09472d>
2012-09-30 23:03:26 +0000

    master flat  updated

--- trunk/masterFlat.py
+++ trunk/masterFlat.py
@@ -6,31 +6,33 @@ J. Hao @ FNAL, 9/30/2012
 """
 import numpy as np
 import pyfits as pf
-import sys
+import sys,time
 sys.path.append('/usr/remote/user/sispi/jiangang/decam-fermi')
 import glob as gl
 from DECamCCD_def import *
 
 
+
 if len(sys.argv) == 1:
     print 'syntax: '
-    print 'For a given set of images: masterFlat FlatFileHead expids masterBiasName'
-    print 'example: masterBias decam_09302012 (12345,12346,12347) masterBias.fits'
+    print 'For a given set of images: masterFlat masterBiasName FlatFileHead expids '
+    print 'example: masterFlat masterBias.fits decam 12345 12346 12347'
     print 'The resulting median image will be named as masterFlat.fits'
 else:
-    filehead = sys.argv[1]
-    bias = sys.argv[2]
-    expid = eval(sys.argv[2])
-    nimg=len(expid)
-    hdu = pf.open(filehead+str(expid[0])+'.fits.fz')
+    startTime=time.time()
+    bias = sys.argv[1]
+    filehead = sys.argv[2]
+    nimg=len(sys.argv) - 3
+    hdu = pf.open(filehead+'_'+sys.argv[3]+'.fits') # need to funpack first
     for ext in range(1,63):
         for j in range(0,nimg):
             b=[]
-            filename = filehead+'_'+str(expid[j])+'.fits.fz'
+            filename = filehead+'_'+sys.argv[2+j]+'.fits'
             imgext = pf.getdata(filename,ext)
             imgosub = oscanSub(imgext)
-            imgosub = imgosub - pf.getdata(bias,ext)
+            imgosub = imgosub - pf.getdata(bias,ext)    
             b.append(imgosub)
         hdu[ext].data=np.median(b,axis=0)
+        hdu[ext].data = hdu[ext].data / robust_mean(hdu[ext].data)
         hdu[ext].header.update('bzero',0)
     hdu.writeto('masterFlat.fits')
